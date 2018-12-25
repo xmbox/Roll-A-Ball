43f576b6dfe7aa66a2e79c38110f17b172fdac87
Mbosoft <mbosoft@mbosofts-mbp.mynet>
2018-12-25 23:40:35 +0300

    First initial

---
+++ Assets/Kodlar/KameraKontrol.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KameraKontrol : MonoBehaviour {
+
+    //topuma ulasabilmek icin public tanimlayip unity panelinden topu tasidik.
+    public GameObject top;
+
+    //aradaki mesafeyi bulmak icin degisken
+    Vector3 mesafe;
+
+	void Start () {
+
+        //aradaki mesafeyi bulmak icin degisken kameramin pozisyonundan topunkini cikariyoruz.
+        mesafe = transform.position - top.transform.position;
+		
+	}
+	
+
+	void LateUpdate () {
+
+        transform.position = top.transform.position + mesafe;
+		
+	}
+}
---
+++ Assets/Kodlar/TopKontrol.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TopKontrol : MonoBehaviour {
+
+    //degiskeni burada olusturuyoruz ki buradan erisilebilsin
+    Rigidbody fizik;
+    public int hiz;
+    int sayac = 0;
+    //panelden toplanilacak bonus sayisini gircegimiz icin public tanimladik
+    public int toplamBonus;
+
+    public Text SayacText;
+    public Text FinishText;
+
+	void Start () {
+
+        fizik = GetComponent<Rigidbody>();
+	}
+
+    // Fiziksel islemlerde FixedUpdate kullancaz
+    void FixedUpdate () {
+
+        //mousun sag ve sol tiklarini ayarlayip yonleri belirliyoruz
+        float yatay = Input.GetAxisRaw("Horizontal");
+        float dikey = Input.GetAxisRaw("Vertical");
+
+        Vector3 vec = new Vector3(yatay,0,dikey);
+
+        fizik.AddForce(vec*hiz);
+		
+	}
+
+    // Nesneye dokunma işlevi OnTriggerEnter
+    //OnTriggerEnter - bir defa dokunur dokunmaz teması belirtiyor
+    //OnTriggerStay - Temas olduğu sürece bizi uyarıyor
+    //OnTriggerExit - Temastan çıkıldıktan sonra yazdırıyor
+    private void OnTriggerEnter(Collider other)
+    {
+        //Destroy kaldırma- silme
+        //Destroy(other.gameObject);
+
+        if (other.gameObject.tag == "Bonus")
+        {
+            // Destroy(other.gameObject);
+
+            // Bu kodla aktifliğini kapatıypruz
+            other.gameObject.SetActive(false);
+            sayac++;
+            SayacText.text = "Sayac = " + sayac;
+
+            if (sayac==toplamBonus)
+            {
+                FinishText.text = "GAME OVER";
+            }
+        }
+    }
+
+}
